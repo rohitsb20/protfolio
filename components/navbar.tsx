3406ba89981b0cf5652cfdb59ac3201c1b56a8e4
rohit <rohitsb96134@gmail.com>
2024-11-25 01:15:55 +0530

    Refactor Navbar component layout and add logo link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>navbar
-        
+    <div className='h-full px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex items-center justify-between'>
+       <div className=''>
+        <Link href='/'>
+        logo
+        </Link>
+       </div>
     </div>
   )
 }
